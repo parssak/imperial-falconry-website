00a95435fe5c9c05a8c301b44d4807ea153bc66b
parssak <parssak@gmail.com>
2022-06-05 10:41:48 -0400

    contact first pass

--- src/views/contact/index.vue
+++ src/views/contact/index.vue
@@ -1,16 +1,14 @@
 <template>
   <Header />
 
-  <Container>
-    <h2>Content</h2>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget
-      nisl auctor, rhoncus nisl ut, aliquam erat.
-    </p>
-  </Container>
+  <Texture>
+    <ContactCard />
+  </Texture>
 </template>
 
 <script setup>
 import Header from "@/components/base/Header.vue";
 import Container from "@/components/layouts/Container.vue";
+import Texture from "@/components/base/Texture.vue";
+import ContactCard from "@/components/layouts/contact/ContactCard.vue";
 </script>
